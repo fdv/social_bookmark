2ea7188285929d77d5f35390f51636bf3e9dbff7
Frédéric de Villamil <frederic@de-villamil.com>
2009-03-23 00:40:50 +0100

    Completely rewrite the controller and helper. Now controller controls, and helper generates HTML. As we say in french, ne mélangeons pas les torchons et les serviettes

--- lib/social_bookmark.rb
+++ lib/social_bookmark.rb
@@ -1,32 +1,44 @@
 # SocialBookmark
 module SocialBookmark
   require 'rexml/document'
+  
+  class SocialItem < Struct.new(:url, :name, :image)
+    def to_s; title end
+  end
 
   def parse_config(permalink, title)
     xml = REXML::Document.new(File.open("#{RAILS_ROOT}/config/sites_EN.xml"))
-
-    output << "<p id='social'>"
+    items = []
 
     REXML::XPath.each(xml, "social_sites/site/") do |elem|
-      bookmark = REXML::XPath.match(elem, "name/text()").first.value
-      url = REXML::XPath.match(elem, "url/text()").first.value.gsub('{link}', permalink).gsub('{title}', CGI::escape(title))
-      image = REXML::XPath.match(elem, "img/text()").first.value
-
-      output << "<span>"
-      output << "<img src='/images/social_bookmark/#{image}' alt='Add to #{bookmark}' /> "
-      output << "<a href='#{url}'>#{bookmark}</a>"
-      output << "</span> "
+      item        = SocialItem.new
+      item.name   = REXML::XPath.match(elem, "name/text()").first.value rescue ""
+      item.url    = REXML::XPath.match(elem, "url/text()").first.value.gsub('{link}', 
+                    permalink).gsub('{title}', CGI::escape(title)) rescue ""
+      item.image  = REXML::XPath.match(elem, "img/text()").first.value rescue ""
+      
+      items << item
     end
     
-    output << "</p>"
-    
-    return output
+    items.sort_by { |item| item.name }
   end
-  
 end
 
 module SocialBookmarkHelper
   def render_social_bookmarks(permalink, title)
-    @controller.parse_config(permalink, title)
+    items = @controller.parse_config(permalink, title)
+    
+    output = "<p id='social'>"
+    
+    items.each do |item|
+      output << "<span>"
+      output << "<a href='#{item.url}'>"
+      output << "<img src='/images/social_bookmark/#{item.image}' alt='Add to #{item.name}' /> "
+      output << "#{item.name}"
+      output << "</a>"
+      output << "</span> "
+    end
+
+    output << "</p>"
   end
 end
